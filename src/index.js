ad1442582b1fd7f87f974dd7879fee8f7811b736
Courtney Noonan <courtneynoonan@me.com>
2017-09-05 13:37:11 -0400

    creating the redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+// invoke createStore with the imported reducer to create the Redux store
+const store = createStore(reducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
